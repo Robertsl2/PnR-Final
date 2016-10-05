f1562f3a37484187033b23ada4e9fdffe0325a10
robertsl <robertslouie1@gmail.com>
2016-10-05 08:56:04 -0400

    started student dance

--- student.py
+++ student.py
@@ -46,7 +46,16 @@ class GoPiggy(pigo.Pigo):
     def dance(self):
         print("Piggy dance")
         ##### WRITE YOUR FIRST PROJECT HERE
-        self.encR(18)
+        for x in range(100, 200, 25):
+            print('speed is set to:' + str(x))
+            set_speed(x)
+            self.encB(5)
+            self.encR(2)
+            self.encL(2)
+            self.encF(5)
+            servo(120)
+            time.sleep(.1)
+
 
     # AUTONOMOUS DRIVING
     def nav(self):
