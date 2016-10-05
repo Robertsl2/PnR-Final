27c70bb5cf617a4462fe8b72a1ddfdade8ae480b
robertsl <robertslouie1@gmail.com>
2016-10-05 09:52:36 -0400

    started student dance

--- pigo.py
+++ pigo.py
@@ -145,7 +145,7 @@ class Pigo(object):
             time.sleep(.01)
 
     def isClear(self) -> bool:
-        for x in range((self.MIDPOINT - 35), (self.MIDPOINT + 35), 5):
+        for x in range((self.MIDPOINT - 50), (self.MIDPOINT + 50), 5):
             servo(x)
             time.sleep(.1)
             scan1 = us_dist(15)
