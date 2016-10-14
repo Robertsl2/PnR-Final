ccd88f13e4d7bef09e30905323b8979f16b3fabd
robertsl <robertslouie1@gmail.com>
2016-10-14 09:32:57 -0400

    Complete clear def

--- student.py
+++ student.py
@@ -69,7 +69,7 @@ class GoPiggy(pigo.Pigo):
 
     # Complete Clear Check
     def completeClear(self):
-        t=9
+        t=10
         print("let's check 0 degrees")
         if not self.isClear():
             return False
