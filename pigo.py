c83177e50c287d3869ab4b9f0cae22087cb0304d
robertsl <robertslouie1@gmail.com>
2016-10-28 09:45:47 -0400

    scan faster

--- pigo.py
+++ pigo.py
@@ -147,7 +147,7 @@ class Pigo(object):
             time.sleep(.01)
 
     def isClear(self) -> bool:
-        for x in range((self.MIDPOINT - 15), (self.MIDPOINT + 15), 5):
+        for x in range((self.MIDPOINT - 50), (self.MIDPOINT + 50), +15):
             servo(x)
             time.sleep(.1)
             scan1 = us_dist(15)
